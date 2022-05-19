db2225eae79ad452f091c1600bde5df94f329290
gguedes <gguedes@student.42.rio>
2022-05-19 18:30:14 -0300

    first push

---
+++ src/commands/info/help.js
@@ -0,0 +1,18 @@
+const Command = require('../../structures/Command')
+
+module.exports = class extends Command {
+	constructor(client) {
+		super(client, {
+			name: 'help',
+			description: 'Manda uma mensagem no privado descrevendo detalhadamente cada comando'
+		})
+	}
+
+	run = (interaction) => {
+		interaction.reply({
+			content: 'Help enviado no privado',
+			ephemeral: true
+		})
+		interaction.user.send('Comando help não esta pronto.')
+	}
+}
